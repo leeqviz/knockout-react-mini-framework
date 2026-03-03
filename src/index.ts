2bd680b6977117e8791fc60c6d9cd6e0a9bde225
Artsiom Farnel <leeqviz@gmail.com>
2026-03-03 08:41:16 +0100

    add husky

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import ko from "knockout";
-import "./index.css";
-import { AppViewModel } from "./lib/models/app";
+import ko from 'knockout';
+import './index.css';
+import { AppViewModel } from './lib/models/app';
 
-import "./lib/bindings/datepicker"; // Инициализируем наш мост для datepicker
-import "./lib/bindings/main"; // Инициализируем наш мост для main
+import './lib/bindings/datepicker'; // Инициализируем наш мост для datepicker
+import './lib/bindings/main'; // Инициализируем наш мост для main
 
 // Запускаем Knockout
 ko.applyBindings(new AppViewModel());
+
+const a: any = 123;
